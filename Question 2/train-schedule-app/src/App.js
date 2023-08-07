070fdd34fd8f2460cdcde9c596232c4df029350f
varunyadavgg <zvarun747@gmail.com>
2023-08-07 21:25:55 +0530

    Update App.js

--- Question 2/train-schedule-app/src/App.js
+++ Question 2/train-schedule-app/src/App.js
@@ -1,11 +1,33 @@
-// App.js
 
 import React, { useState } from "react";
 import RegistrationForm from "./components/RegistrationForm";
 import AuthenticationForm from "./components/AuthenticationForm";
 import TrainDetails from "./components/TrainDetails";
+import {BrowserRouter as Router,Route} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import{Routes} from 'react-router-dom';
 
 function App() {
+
+  <Router>
+<Navbar>
+<Routes>
+<Route path="/RegistrationForm" element={<RegistrationForm />} >
+    
+    
+  </Route> 
+
+  <Route path="/AuthenticationForm" element={<AuthenticationForm /> }> 
+    
+  </Route> 
+
+  <Route path="/TrainDetails" element={<TrainDetails />}>
+    
+  </Route> 
+</Routes>
+</Navbar>
+
+</Router>
   const [registrationData, setRegistrationData] = useState(null);
   const [authToken, setAuthToken] = useState(null);
 
@@ -25,6 +47,8 @@ function App() {
         <TrainDetails trainID="2344" authToken={authToken} />
       )}
     </div>
+
+
   );
 }
 
